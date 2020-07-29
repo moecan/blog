93b6c1993cb1867542c3c095ea891eebc116ef85
moecan <58144700+moecan@users.noreply.github.com>
2020-07-29 22:49:41 +0700

    Update index_2.js

--- index_2.js
+++ index_2.js
@@ -22,12 +22,27 @@ $(document).ready(function(){
 		$(this).attr("href", VS1); 
   		var Server1 = document.getElementById("LS1").href;
 		$(this).attr("style", "display:none;"); 
-    		$('#ListServer').html( ' <button id="Serv1">Server1</button> ' );
+    		$('#ListServer').html( ' <button id="Serv1">Server Alternatif</button> ' );
     			$("#Serv1").click(function(){
 			var vbuka = ' pause controls controlsList="nodownload" oncontextmenu="return false;" style="width:100%;height:270px;">\n'+
 			'<source src="';
 			var vtutup = '" type="video/mp4"></video>';
 			$('#iframeHolder').html( '<video ' + vbuka + Server1  + vtutup );
-		});
+			});
   });
+  $('a[href^="http://hentai.dx.am/id?=2"]').each(function(){
+    var LS1 = $(this).attr("href"); 
+		var VS2 = LS2.replace("http://hentai.dx.am/id?=2&v=", creampie); 
+		$(this).attr("id", "LS2"); 
+		$(this).attr("href", VS2); 
+  		var Server2 = document.getElementById("LS2").href;
+		$(this).attr("style", "display:none;"); 
+    		$('#ListServer2').html( ' <button id="Serv2">Server Aman</button> ' );
+    			$("#Serv2").click(function(){
+			var vbuka = ' pause controls controlsList="nodownload" oncontextmenu="return false;" style="width:100%;height:270px;">\n'+
+			'<source src="';
+			var vtutup = '" type="video/mp4"></video>';
+			$('#iframeHolder').html( '<video ' + vbuka + Server2  + vtutup );
+			});
+  });	
 });
