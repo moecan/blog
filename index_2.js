9291ea2ecc5ec0c4403a5e7e7edbce30caa59d05
moecan <58144700+moecan@users.noreply.github.com>
2020-07-29 22:13:14 +0700

    Update and rename index_1.js to index_2.js

--- index_2.js
+++ index_2.js
@@ -20,9 +20,14 @@ $(document).ready(function(){
 		var VS1 = LS1.replace("http://hentai.dx.am/id?=1&v=", creampie); 
 		$(this).attr("id", "LS1"); 
 		$(this).attr("href", VS1); 
-  	var Server1 = document.getElementById("LS1").href;
+  		var Server1 = document.getElementById("LS1").href;
 		$(this).attr("style", "display:none;"); 
-    $('#ListServer').html( ' <button >Server1</button> ' );
-    
+    		$('#ListServer').html( ' <button id="Serv1">Server1</button> ' );
+    			$("#Serv1").click(function(){
+			var vbuka = ' pause controls controlsList="nodownload" oncontextmenu="return false;" style="width:100%;height:270px;">\n'+
+			'<source src="';
+			var vtutup = '" type="video/mp4"></video>';
+			$('#iframeHolder').html( '<video ' + vbuka + Server1  + vtutup );
+		});
   });
 });
